3074183bf1b6475ded908331404217caafd97667
Motie <50115514+M0tie@users.noreply.github.com>
2019-10-27 11:38:03 -0700

    Add files via upload

--- mpv_mods/install.sh
+++ mpv_mods/install.sh
@@ -5,3 +5,4 @@ sudo cp /home/pi/dok/mpv_mods/d.sh /home/pi/
 sudo chmod 755 ./d.sh
 sudo systemctl enable /etc/systemd/user/vita.service
 sudo systemctl start /etc/systemd/user/vita.service
+sudo reboot
