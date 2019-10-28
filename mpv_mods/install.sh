99f097366249a80cb749da1fe74c9a17fa25a28a
Motie <50115514+M0tie@users.noreply.github.com>
2019-10-28 17:55:53 +0200

    Delete install.sh

--- mpv_mods/install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-sudo cp /home/pi/dok/mpv_mods/Etc_Systemd_user/* /etc/systemd/user/
-sudo cp /home/pi/dok/mpv_mods/etc_udev/* /etc/udev/rules.d/
-sudo cp /home/pi/dok/mpv_mods/d.sh /home/pi/
-sudo chmod 755 ./d.sh
-sudo systemctl enable /etc/systemd/user/vita.service
-sudo systemctl start /etc/systemd/user/vita.service
-sudo reboot
