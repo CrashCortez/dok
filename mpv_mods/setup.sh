4ec0cb1bad9c5bdcb482b90aca29ce34c290facc
Motie <50115514+M0tie@users.noreply.github.com>
2019-10-29 18:36:25 -0700

    Add files via upload

--- mpv_mods/setup.sh
+++ mpv_mods/setup.sh
@@ -3,5 +3,3 @@ sudo cp /home/pi/dok/mpv_mods/Etc_Systemd_user/* /etc/systemd/user/
 sudo cp /home/pi/dok/mpv_mods/etc_udev_rules.d/* /etc/udev/rules.d/
 sudo cp /home/pi/dok/mpv_mods/close.sh /home/pi/
 sudo cp /home/pi/dok/mpv_mods/vidware /home/pi/
-sudo chmod 755 close.sh
-sudo chmod 755 vidware
